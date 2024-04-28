c96afa2afaccd655b119c7652002f20744de4e3b
Chris Gregori <chrisgreg23@googlemail.com>
2024-04-28 19:53:41 +0100

    Made simpler

--- lib/tasks/install.ex
+++ lib/tasks/install.ex
@@ -6,7 +6,7 @@ defmodule Mix.Tasks.Bloom.Install do
   def run(args) do
     case args do
       ["glow_button"] ->
-        install_component("GlowButton")
+        install_component("glow_button")
 
       _ ->
         Mix.shell().info("Usage: mix bloom.install [component_name]")
@@ -14,43 +14,30 @@ defmodule Mix.Tasks.Bloom.Install do
     end
   end
 
-  defp install_component(component_name) do
-    component_name = Macro.underscore(component_name)
-    module = String.to_atom("Bloom.Components.#{component_name}")
+  defp install_component(file_name) do
     project_name = Mix.Project.config()[:app] |> Atom.to_string() |> String.downcase()
 
-    case retrieve_source_code(module) do
+    case retrieve_source_code(file_name) do
       {:ok, source_code} ->
         component_dir = component_dir(project_name)
-
-        # Create component directory if it doesn't exist
         File.mkdir_p(component_dir)
-        target_path = "#{component_dir}/#{component_name}.ex"
+        target_path = "#{component_dir}/#{file_name}.ex"
 
-        # Replace component name in source code
         module_name = project_name |> Macro.camelize()
 
         updated_source_code =
           Regex.replace(~r/Bloom\.Components/, source_code, "#{module_name}Web.Components")
 
         File.write!(target_path, updated_source_code)
-        Mix.shell().info("#{component_name} component installed successfully.")
+        Mix.shell().info("#{file_name} component installed successfully.")
 
       {:error, reason} ->
         Mix.shell().info("Error: #{reason}")
     end
   end
 
-  defp retrieve_source_code(module) do
-    # Assuming the module is in the "Bloom.Components" namespace
-    module_name =
-      Atom.to_string(module)
-      |> String.split(".")
-      |> List.last()
-      |> Macro.underscore()
-
-    # Assuming the source files are in the "lib/bloom/components" directory
-    source_file = "lib/bloom/components/#{String.downcase(module_name)}.ex"
+  defp retrieve_source_code(file_name) do
+    source_file = "lib/bloom/components/#{file_name}.ex"
 
     if File.exists?(source_file) do
       {:ok, File.read!(source_file)}
