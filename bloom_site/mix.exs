4c62a1bf7b5b24b509f36057f4f513a5b99c6f3b
Chris Gregori <chrisgreg23@googlemail.com>
2024-05-02 20:16:00 +0100

    Fix bento dark mode and card dark mode styling

--- bloom_site/mix.exs
+++ bloom_site/mix.exs
@@ -54,7 +54,7 @@ defmodule BloomSite.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:bloom, "~> 0.0.5"},
+      {:bloom, "~> 0.0.6"},
       {:phoenix_storybook, "~> 0.6.0"}
     ]
   end
--- lib/bloom/components/bento_grid.ex
+++ lib/bloom/components/bento_grid.ex
@@ -14,7 +14,7 @@ defmodule Bloom.Components.BentoGrid do
 
   def bento_grid(assigns) do
     ~H"""
-    <div class={["grid w-full auto-rows-[16rem] grid-cols-3 gap-4", @class]}>
+    <div class={["flex flex-col md:grid w-full md:auto-rows-[16rem] !grid-cols-1 md:grid-cols-3 gap-4", @class]}>
       <%= render_slot(@inner_block) %>
     </div>
     """
@@ -37,7 +37,8 @@ defmodule Bloom.Components.BentoGrid do
   def bento_card(assigns) do
     ~H"""
     <div class={[
-      "group relative row-span-#{@row_end - @row_start} col-span-#{@col_end - @col_start} row-start-#{@row_start} row-end-#{@row_end} col-start--#{@col_start} col-end-#{@col_end} flex flex-col justify-between overflow-hidden rounded-xl bg-white [box-shadow:0_0_0_1px_rgba(0,0,0,.03),0_2px_4px_rgba(0,0,0,.05),0_12px_24px_rgba(0,0,0,.05)] transform-gpu dark:bg-black dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#ffffff1f_inset]",
+      "group relative row-span-#{@row_end - @row_start} col-span-#{@col_end - @col_start} row-start-#{@row_start} row-end-#{@row_end} col-start--#{@col_start} col-end-#{@col_end} flex flex-col justify-between overflow-hidden rounded-xl bg-white [box-shadow:0_0_0_1px_rgba(0,0,0,.03),0_2px_4px_rgba(0,0,0,.05),0_12px_24px_rgba(0,0,0,.05)]
+      transform-gpu dark:bg-zinc-900 dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#ffffff1f_inset]",
       @class
     ]}>
       <div><%= render_slot(@background) %></div>
