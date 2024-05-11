6e7ddb60d033022b3897f9762317eae31a152007
Chris Gregori <chrisgreg23@googlemail.com>
2024-05-11 17:20:03 +0100

    Remove landing page geneated for bloom_site

--- bloom_site/lib/bloom_site_web/live/landing_page_live.ex
+++
@@ -1,8 +0,0 @@
-defmodule BloomSiteWeb.LandingPageLive do
-  use BloomSiteWeb, :live_view
-  import BloomSiteWeb.Components.{Hero, GradientBlob}
-
-  def mount(_params, _session, socket) do
-    {:ok, socket}
-  end
-end
